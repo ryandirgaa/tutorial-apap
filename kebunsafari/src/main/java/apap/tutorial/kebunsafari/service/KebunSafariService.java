03238054917e7106df053efdda1d4bc263a25686
RYAN DIRGA AIDIL HAKIM <8002971-rayendierga@users.noreply.gitlab.com>
2021-09-21 17:35:29 +0700

    docs: add deletebyname method

--- kebunsafari/src/main/java/apap/tutorial/kebunsafari/service/KebunSafariService.java
+++ kebunsafari/src/main/java/apap/tutorial/kebunsafari/service/KebunSafariService.java
@@ -14,6 +14,11 @@ public interface KebunSafariService {
     //Method untuk mendapatkan sebuah KebunSafari berdasarkan ID yang dimiliki
     KebunSafariModel getKebunSafariByIdKebunSafari(String idKebunSafari);
 
+    KebunSafariModel getKebunSafariByNamaKebunSafari(String namaKebunSafari);
+
     //Method untuk menghapus sebuah KebunSafari berdasarkan ID yang dimiliki
     void deleteKebunSafariByIdKebunSafari(String idKebunSafari);
+
+    //Method untuk menghapus kebun safari berdasarkan nama
+    void deleteKebunSafariByNamaKebunSafari(String namaKebunSafari);
 }
